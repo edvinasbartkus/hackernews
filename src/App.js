6422ea59da7d5fb049abab4ba703b9e892e19c03
Edvinas Bartkus <edvinas@bartkus.lt>
2017-10-11 11:43:46 +0300

    Implement infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Infinite from 'react-infinite';
 import './App.css';
 import Firebase from './utils/Firebase';
 
@@ -6,7 +7,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
+      items: [],
+      isLoading: false
     }
   }
 
@@ -17,33 +19,63 @@ class App extends Component {
   }
 
   componentDidMount() {
-    Firebase.database().ref('v0/newstories').on('value', snap => {
-      snap.val().forEach(id => {
-        Firebase.database().ref('v0/item').child(id).once('value', snapshot => {
-          const item = snapshot.val();
-          if (item && item.title) {
-            this.addChild({ title: item.title });
-          } else {
-            console.error(id);
-          }
-        });
-      });
-    });
+    this.getLatest();
+  }
+
+  getLatest() {
+    this.setState({ isLoading: true });
+    Firebase.database()
+            .ref('v0/newstories')
+            .limitToFirst(100)
+            .once('value', snap => this.onLoad(snap))
+            .then(() => this.setState({ isLoading: false }));
+  }
+
+  getItem(id) {
+    Firebase.database()
+            .ref('v0/item')
+            .child(id)
+            .once('value', snap => this.onItem(snap, id));
+  }
+
+  onLoad(snap) {
+    snap.val().forEach(id => this.getItem(id));
+  }
+
+  onItem(snapshot, id) {
+    const item = snapshot.val();
+    if (item && item.title) {
+      this.addChild({ title: item.title });
+    } else {
+      console.error(id);
+    }
+  }
+
+  onEndReached() {
+    if (!this.state.isLoading) {
+      this.getLatest();
+    }
   }
 
   render() {
     return (
-      <div className="App">
-      {
-        this.state.items.map((item, index) => 
-          (
-            <div key={`item_${index}`}>
-              <span>{item.title}</span>
-            </div>
+      <Infinite elementHeight={18}
+                useWindowAsScrollContainer
+                infiniteLoadBeginEdgeOffset={200}
+                onInfiniteLoad={() => this.onEndReached()}
+                loadingSpinnerDelegate={<span>⏳</span>}
+                isInfiniteLoading={this.state.isLoading}
+      >
+        <ul className="App">
+        {
+          this.state.items.map((item, index) => 
+            (
+              <li key={`item_${index}`}>{item.title}</li>
+            )
           )
-        )
-      }
-      </div>
+        }
+        </ul>
+      </Infinite>
     );
   }
 }
