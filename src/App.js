202392e788db5e099c0c91f3fb53bf4ad3bdfc85
Edvinas Bartkus <edvinas@bartkus.lt>
2017-10-11 11:11:41 +0300

    Dummy example rendering all items fetched from HN

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Firebase from './utils/Firebase';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    }
+  }
+
+  addChild(item) {
+    this.setState({
+      items: this.state.items.concat([ item ])
+    });
+  }
+
+  componentDidMount() {
+    Firebase.database().ref('v0/newstories').on('value', snap => {
+      snap.val().forEach(id => {
+        Firebase.database().ref('v0/item').child(id).once('value', snapshot => {
+          const item = snapshot.val();
+          if (item && item.title) {
+            this.addChild({ title: item.title });
+          } else {
+            console.error(id);
+          }
+        });
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      {
+        this.state.items.map((item, index) => 
+          (
+            <div key={`item_${index}`}>
+              <span>{item.title}</span>
+            </div>
+          )
+        )
+      }
       </div>
     );
   }
